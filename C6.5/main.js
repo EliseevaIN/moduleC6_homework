ad20499d442cdcf130db68a4a5004c9fb9109bdb
EliseevaIN <60147321+EliseevaIN@users.noreply.github.com>
2023-01-26 14:33:22 +0300

    Add files via upload

---
+++ C6.5/main.js
@@ -0,0 +1,8 @@
+const btnNode = document.querySelector('.j-btn');
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+
+btnNode.addEventListener('click', () => {
+    alert(`Ширина экрана: ${screenWidth}. Высота экрана: ${screenHeight}.`)
+})
+
